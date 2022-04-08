9857c810ee7c61b9c2cd69d86ef3b98521414831
Alan Hernández <ahernandez102@ucol.mx>
2022-04-08 15:43:38 -0500

    Added comments

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.use('/assets', express.static(__dirname + '/public'));
+var express = require('express'); //Importando el módulo Express a una variable.
+var app = express(); //Declaramos la app de Express.
+var port = process.env.PORT || 3000; //Asignamos el puerto en el que se alojará el servidor.
+app.use('/assets', express.static(__dirname + '/public')); //Asignando el directorio virtual para contenido estático.
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //Añadimos el Template EJS.
 app.use('/', function(req, res, next) {
-    console.log(`Request Url: ${req.url}`);
+    console.log(`Request Url: ${req.url}`); //Imprimiendo las URLs en nuestra terminal.
     next();
-});
+});  
 
+//Creando nuestro directorio raíz, es decir, nuestra primera ruta (Hello world!).
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index'); //Indicamos la carpeta index.ejs al método render.
 });
 
+//Creando la segunda ruta, la cual nos muestra una lista determinada por el número que ingresemos.
 app.get('/numbers/:number', function(req, res) {
     res.render('numbers', { number: req.params.number });
 });
 
-app.listen(port);
+app.listen(port); //Ponemos el servidor a la escucha de alguna petición.
